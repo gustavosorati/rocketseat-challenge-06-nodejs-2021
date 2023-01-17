d168bead2e8d5d52b981cacb028cfc5ba591e92a
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-01-17 18:27:00 -0300

    Resolução do desafio

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, ILike, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    const user = await this.repository
+      .createQueryBuilder("games")
+      .where("games.title ilike :title", { title: `%${param}%` })
+      .getMany();
+
+    return user
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return this.repository.query(`SELECT COUNT(*) FROM GAMES`); // Complete usando raw query
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    const user = await this.repository
+    .createQueryBuilder("games")
+    .where("games.id = :id", { id: id})
+    .relation(Game, "users")
+    .of(id)
+    .loadMany();
+    
+    return user
+  }
+}
